6782bb3d4b0d8b60e6a73f917cce8dcded4d760b
saharabanu <saharabanungn@gmail.com>
2023-12-09 12:52:37 +0600

    react tailwind hrader added

---
+++ react-agency-website/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Home from './components/first-project/ui/Home'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react-agency-website/src/components/first-project/ui/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+
+    </>
+  )
+}
+
+export default Home
